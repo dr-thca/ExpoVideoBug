2f822c6bcfc8864e1a7f1a39f1cfcfe74526cf47
Theodor René Carlsen <thca@dr.dk>
2025-02-19 10:27:19 +0100

    make better example

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,58 +1,11 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { createContext, ReactNode, useEffect, useRef } from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { createVideoPlayer, VideoPlayer } from "expo-video";
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-type VideoPlayerRef = React.MutableRefObject<VideoPlayer>;
-export const VideoPlayerContext = createContext<VideoPlayerRef>(null as any);
-
-export function PlayerContextProvider({ children }: { children: ReactNode }) {
-  const anotherlink = "https://www.w3schools.com/html/mov_bbb.mp4";
-  const player = useRef<VideoPlayer>(createVideoPlayer(anotherlink));
-
-  return (
-    <VideoPlayerContext.Provider value={player}>
-      {children}
-    </VideoPlayerContext.Provider>
-  );
-}
-
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
-    <PlayerContextProvider>
-      <ThemeProvider value={DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </PlayerContextProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,37 @@
+import { useVideoPlayer, VideoView } from "expo-video";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default function Quad() {
+  const anotherlink = "https://www.w3schools.com/html/mov_bbb.mp4";
+  const videoPlayer = useVideoPlayer(anotherlink);
+  return (
+    <View style={styles.stepContainer}>
+      <VideoView style={styles.video} player={videoPlayer} />
+      <VideoView style={styles.video} player={videoPlayer} />
+      <VideoView style={styles.video} player={videoPlayer} />
+      <VideoView style={styles.video} player={videoPlayer} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  video: {
+    height: "25%",
+    width: "75%",
+  },
+  stepContainer: {
+    margin: 16,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
